ddfafca87dbab8941bf44c50021ce95097d776b5
raffazizzi <raffaeleviglianti@gmail.com>
2014-03-06 17:41:18 -0500

    changes specific to wwa first dmeo

--- shared-canvas/demo/js/main.js
+++ shared-canvas/demo/js/main.js
@@ -1,6 +1,27 @@
 (function ($) { 
   'use strict';
 
-  var sc = new SGASharedCanvas.Application({"manifest":"example-manifest.jsonld"});
+  function GetURLParameter(sParam)
+  {
+      var sPageURL = window.location.search.substring(1);
+      var sURLVariables = sPageURL.split('&');
+      for (var i = 0; i < sURLVariables.length; i++) 
+      {
+          var sParameterName = sURLVariables[i].split('=');
+          if (sParameterName[0] == sParam) 
+          {
+              return sParameterName[1];
+          }
+      }
+  }
+
+  var manifest = GetURLParameter('mf');
+  var manifestURL = "example-manifest.jsonld";
+
+  if (manifest != undefined) {
+    manifestURL = "manifests/"+manifest+"/Manifest.jsonld";
+  }
+
+  var sc = new SGASharedCanvas.Application({"manifest":manifestURL});
 
 })(jQuery);
